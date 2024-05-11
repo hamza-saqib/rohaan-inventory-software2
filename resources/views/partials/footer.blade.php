51bfc47239e915aad863fcfbf831cf8436f5fdda
ROHAAN NADEEM <107777538+Rohaan02@users.noreply.github.com>
2024-05-11 23:42:11 +0500

    Latest Backup Pushed in the requirements folder
    All completed from my side

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="footer">
     <div class="pull-right">
-        support@malhoc.com
+        webdevelopers4u2u@gmail.com
     </div>
     <div>
-        <strong>Copyright</strong> Malhoc &copy; 2024
+        <strong>Copyright</strong> OCEAN MARKET &copy; 2024
     </div>
 </div>
