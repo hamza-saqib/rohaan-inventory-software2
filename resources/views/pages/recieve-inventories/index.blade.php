0fe0a303fb130e7219852229b84d3ec30212dbe0
ROHAAN NADEEM <107777538+Rohaan02@users.noreply.github.com>
2024-07-13 15:29:27 +0500

    Inventory Reciept Pagination error
    pagination dont work accurately when we filter the data and want to move next page from pagination of that filtered data it redirect to the next page of unfiltered data

--- resources/views/pages/recieve-inventories/index.blade.php
+++ resources/views/pages/recieve-inventories/index.blade.php
@@ -244,7 +244,7 @@
                                         </tr> --}}
                                     </tfoot>
                                 </table>
-                                {{ $inventories->links('vendor.pagination.bootstrap-5') }}
+                                {{ $inventories->appends(request()->query())->links('vendor.pagination.bootstrap-5') }}
                             </div>
 
                         </div>
